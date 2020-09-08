12b1f75eeeecb7fd84c0c1666405931c1f75a553
Ankit Jaiswal <ankitjaiswal.998@gmail.com>
2020-09-08 22:07:55 +0530

    Test again

--- src/testcases/LoginTestCase.java
+++ src/testcases/LoginTestCase.java
@@ -14,7 +14,6 @@ public class LoginTestCase extends BaseTest {
 	public void LIA() {
 
 		driver.findElement(By.name("username")).sendKeys("ankit.jaiswal");
-		
 		driver.findElement(By.name("password")).sendKeys("Summer@12345");
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
 
